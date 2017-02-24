78426608da6059584e037e8d698dc6dba3abcb17
piorpua <yaxiong@mgtv.com>
2017-02-24 15:09:54 +0800

    +) DemoActivity

---
+++ app/src/main/java/cn/piorpua/baselib/demo/MainActivity.java
@@ -0,0 +1,21 @@
+package cn.piorpua.baselib.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.piorpua.lib.R;
+
+/**
+ * Author: piorpua<helloworld.hnu@gmail.com><br>
+ * Date Created: 17/2/24
+ *
+ * <p>Brief: Demo Activity</p>
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main_act);
+    }
+}
